cc3543767510d27b8e547129ee25747630fa12ec
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2026-09-05 15:56:45 +0500

    update environment

--- config/settings.py
+++ config/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5r2942(@bsai$--%jcd-y3za#yb$+_bv8+9fj)%jioqr(!seh8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = []
 
